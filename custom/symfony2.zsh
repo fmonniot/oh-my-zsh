a8493f45f9eb4c422d905f9e9d68b792672cd884
François Monniot <francoismonniot@gmail.com>
2012-09-19 19:09:57 +0200

    Fix

--- custom/symfony2.zsh
+++ custom/symfony2.zsh
@@ -1,11 +1,11 @@
 # Symfony2 basic command completion
 
 _symfony2_get_command_list_custom () {
-	sf --no-ansi | sed "1,/Available commands/d" | awk '/^  [a-z]+/ { print $1 }'
+	$SETTINGS/bin/sf --no-ansi | sed "1,/Available commands/d" | awk '/^  [a-z]+/ { print $1 }'
 }
 
 _symfony2_custom () {
-  if [ -f sf ]; then
+  if [ -f $SETTINGS/bin/sf ]; then
     compadd `_symfony2_get_command_list_custom`
   fi
 }
