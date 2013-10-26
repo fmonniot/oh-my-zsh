5359331a25c8a1357a3d7411018e449ddd7e3ea1
François Monniot <francoismonniot@gmail.com>
2013-10-26 11:42:02 +0200

    move some aliases to oh-my-zsh repo

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -1,1 +1,8 @@
+alias zshconfig="nano ~/.zshrc"
+
 alias code="/usr/share/vim/vim74/macros/less.sh"
+alias gitg="gitg 2>/dev/null &"
+
+alias start-vpn-minet="sudo systemctl start openvpn@minet.service"
+alias stop-vpn-minet="sudo systemctl stop openvpn@minet.service"
+
